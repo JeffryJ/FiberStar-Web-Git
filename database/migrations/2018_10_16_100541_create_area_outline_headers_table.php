f22fde3baa00f42658e1687a7848e6bff0e2589c
Unknown <jeffry24797@gmail.com>
2018-11-20 20:01:27 +0700

    migration file fix

--- database/migrations/2018_10_16_100541_create_area_outline_headers_table.php
+++ database/migrations/2018_10_16_100541_create_area_outline_headers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOutlineHeadersTable extends Migration
+class CreateAreaOutlineHeadersTable extends Migration
 {
     /**
      * Run the migrations.
